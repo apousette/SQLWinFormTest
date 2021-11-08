29858c2d3e94a1d32093713f0dae29de6e8348c1
apousette <adam021@live.se>
2021-11-08 15:12:42 +0100

    exercise done

--- test/Form1.Designer.cs
+++ test/Form1.Designer.cs
@@ -29,13 +29,81 @@ namespace test
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(32, 64);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(335, 356);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(505, 63);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(487, 356);
+            this.listBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 482);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Name";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(225, 482);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(108, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Surname";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(399, 482);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Add Person";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1159, 843);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- test/Form1.cs
+++ test/Form1.cs
@@ -15,6 +15,45 @@ namespace test
         public Form1()
         {
             InitializeComponent();
+
+            var MyPeople = new PeopleEntities().People;
+
+            foreach(Person item in MyPeople)
+            {
+                listBox1.Items.Add(item);
+            }
+
+            listBox1.SelectedIndexChanged += new EventHandler(people_SelectedIndexChanged);
+
+        }
+
+        private void people_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox ListofPeople = sender as ListBox;
+            Person SelectedPerson = ListofPeople.SelectedItem as Person;
+
+            listBox2.Items.Add($"{SelectedPerson.name} {SelectedPerson.surname}");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var context = new PeopleEntities())
+            {
+                var newperson = new Person {name = textBox1.Text, surname = textBox2.Text} ;
+                context.People.Add(newperson);
+                context.SaveChanges();
+            }
         }
     }
+
+    public partial class Person
+    {
+        public override string ToString()
+        {
+            return $"{name} {surname}";
+        }
+
+    }
+
 }
